c9f9f23ec4761b8a91be79c780879cf504d6d0db
Gabriel Escame <gabriel.escame@gmail.com>
2021-06-29 03:18:09 -0300

    feat: formatDate util

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,12 @@
 export const dateToApi = (date) => {
-  const formatedDate = new Intl.DateTimeFormat("pt-br")
-    .format(date)
-    .split("/");
+  const formatedDate = new Intl.DateTimeFormat("pt-br").format(date).split("/");
   const [day, month, year] = formatedDate;
   const dateApi = `${year}-${month}-${day}`;
   return dateApi;
+};
+
+export const formatDate = (stringDate = "") => {
+  const date = new Date(stringDate);
+  const formatedDate = new Intl.DateTimeFormat("en-US").format(date);
+  return formatedDate;
 };
